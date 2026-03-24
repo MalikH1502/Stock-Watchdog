635e891edd626c41d60c2d934c0dfe0bd32e9746
Malik Halabi <malik.halabi@students.mq.edu.au>
2026-03-24 12:23:10 +1100

    Initial Commit.

---
+++ StockWatchdog/StockWatchdog/src/test/java/com/malikh/StockWatchdog/StockWatchdogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.malikh.StockWatchdog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockWatchdogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
