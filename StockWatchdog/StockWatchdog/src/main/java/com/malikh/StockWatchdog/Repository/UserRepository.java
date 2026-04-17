71e1d9b90ecaee5cbcada3744fdbccb5297d75e8
Malik Halabi <malik.halabi@students.mq.edu.au>
2026-04-17 12:31:00 +1000

    Fixed static field and constructor.

--- StockWatchdog/StockWatchdog/src/main/java/com/malikh/StockWatchdog/Repository/UserRepository.java
+++ StockWatchdog/StockWatchdog/src/main/java/com/malikh/StockWatchdog/Repository/UserRepository.java
@@ -3,7 +3,9 @@ package com.malikh.StockWatchdog.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.malikh.StockWatchdog.Entity.User;
+
 @Repository
-public interface UserRepository extends JpaRepository<String, Long>{
+public interface UserRepository extends JpaRepository<User, Long>{
     
 }
--- StockWatchdog/StockWatchdog/src/main/java/com/malikh/StockWatchdog/Service/UserService.java
+++ StockWatchdog/StockWatchdog/src/main/java/com/malikh/StockWatchdog/Service/UserService.java
@@ -9,9 +9,8 @@ import com.malikh.StockWatchdog.Repository.UserRepository;
 @Service
 public class UserService {
     @Autowired
-    private static UserRepository userRepo;
-
-    public static void createUser(User u){
+    private UserRepository userRepo;
+    public void createUser(User u){
         userRepo.save(u);
     }
 }
