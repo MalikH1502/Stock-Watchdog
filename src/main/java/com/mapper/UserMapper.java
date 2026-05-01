028b3404a13db62f27c417673bffae72d0e7fb20
Malik Halabi <malik.halabi@students.mq.edu.au>
2026-05-01 16:08:11 +1000

    Add AlertMapper and UserMapper classes for DTO conversion

---
+++ src/main/java/com/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.mapper;
+
+import org.springframework.stereotype.Component;
+
+import com.malikh.stockwatchdog.dto.UserDTO;
+import com.malikh.stockwatchdog.entity.User;
+
+@Component
+public class UserMapper {
+    public UserDTO toDTO(User user) {
+        UserDTO dto = new UserDTO();
+        dto.setId(user.getId());
+        dto.setUsername(user.getUsername());
+        return dto;
+    }
+}
