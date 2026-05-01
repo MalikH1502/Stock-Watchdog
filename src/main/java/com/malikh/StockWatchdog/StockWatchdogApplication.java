3e62895e04f49cf53a5422076438d46a8bf41310
Malik Halabi <malik.halabi@students.mq.edu.au>
2026-05-01 16:06:41 +1000

    Fix package name casing for StockWatchdogApplication

--- src/main/java/com/malikh/StockWatchdog/StockWatchdogApplication.java
+++ src/main/java/com/malikh/StockWatchdog/StockWatchdogApplication.java
@@ -1,4 +1,4 @@
-package com.malikh.StockWatchdog;
+package com.malikh.stockwatchdog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
