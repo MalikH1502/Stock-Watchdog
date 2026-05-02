26050fd677e83142ac2fbc180eefbbdacbb27ee6
Malik Halabi <malik.halabi@students.mq.edu.au>
2026-05-02 16:14:54 +1000

    Refactor MyController to use constructor injection and update stock retrieval methods to return StockDTO

--- src/main/java/com/malikh/StockWatchdog/Controllers/MyController.java
+++ src/main/java/com/malikh/StockWatchdog/Controllers/MyController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,11 +15,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.malikh.stockwatchdog.repository.UserRepository;
+import com.malikh.stockwatchdog.dto.StockDTO;
 import com.malikh.stockwatchdog.entity.Alert;
 import com.malikh.stockwatchdog.entity.Stock;
 import com.malikh.stockwatchdog.entity.User;
 import com.malikh.stockwatchdog.entity.Stock;
 import com.malikh.stockwatchdog.entity.WatchlistEntry;
+import com.malikh.stockwatchdog.mapper.StockMapper;
 import com.malikh.stockwatchdog.repository.AlertRepository;
 import com.malikh.stockwatchdog.repository.StockRepository;
 import com.malikh.stockwatchdog.repository.WatchlistRepository;
@@ -29,27 +32,33 @@ import com.malikh.stockwatchdog.service.WatchlistService;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "*")
 public class MyController {
-
-    @Autowired
     private StockService stockService;
-    @Autowired
     private WatchlistService watchService;
-    @Autowired
     private AlertService alertService;
-    @Autowired
     private UserService userService;
+    private StockMapper stockMapper;
+
+    public MyController(StockService stockService, WatchlistService watchService, AlertService alertService, UserService userService, StockMapper stockMapper) {
+        this.stockService = stockService;
+        this.watchService = watchService;
+        this.alertService = alertService;
+        this.userService = userService;
+        this.stockMapper = stockMapper;
+    }
+ 
 
     // Return all stocks
     @GetMapping("/stocks")
-    public List<Stock> retrieveStockList() {
+    public List<StockDTO> retrieveStockList() {
         return stockService.getAllStocks();
     }
 
     // Return SPECIFIC stock
-    @GetMapping("/stocks/{symbol}")
-    public Optional<Stock> findByStock(@PathVariable String symbol) {
-        return stockService.findById(symbol);
+    @GetMapping("/stocks/{id}")
+    public Optional<StockDTO> findByStock(@PathVariable Long id) {
+        return stockService.findById(id);
     }
 
 
@@ -72,19 +81,20 @@ public class MyController {
     
 
     @PostMapping("/stocks")
-        public void createStock(@RequestBody Stock s){
+        public Stock createStock(@RequestBody Stock s){
             stockService.createStock(s);
+            return s;   
         }
     
 
-    @DeleteMapping("/stocks/{symbol}")
-        public void deleteStockById(@PathVariable String symbol){
-            stockService.deleteStock(symbol);}
+    @DeleteMapping("/stocks/{id}")
+        public void deleteStockById(@PathVariable Long id){
+            stockService.deleteStock(id);}
         
 
 
-    @PutMapping("/stocks/{symbol}")
-        public void updateStock(@PathVariable String symbol, @RequestBody Stock s){
-            stockService.updateStock(symbol, s);
+    @PutMapping("/stocks/{id}")
+        public void updateStock(@PathVariable Long id, @RequestBody Stock s){
+            stockService.updateStock(id, s);
         }
     }
