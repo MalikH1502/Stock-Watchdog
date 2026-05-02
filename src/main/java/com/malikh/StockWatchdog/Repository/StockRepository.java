5a976320ad21f98bb99c218990a74969181a9cfe
Malik Halabi <malik.halabi@students.mq.edu.au>
2026-05-02 16:17:35 +1000

    Refactor StockRepository to use Long as the ID type for Stock entity

--- src/main/java/com/malikh/StockWatchdog/Repository/StockRepository.java
+++ src/main/java/com/malikh/StockWatchdog/Repository/StockRepository.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Repository;
 import com.malikh.stockwatchdog.entity.Stock;
 
 @Repository
-public interface StockRepository extends JpaRepository<Stock, String> {
+public interface StockRepository extends JpaRepository<Stock, Long> {
     
 }
