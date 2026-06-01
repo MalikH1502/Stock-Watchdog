99b81de0c0b1186baf8514de636b383d904af9fa
Malik Halabi <malik.halabi@students.mq.edu.au>
2026-06-01 17:11:23 +1000

    Remove email validation annotation from UserRegistrationRequest DTO

--- src/main/java/com/malikh/StockWatchdog/dto/UserRegistrationRequest.java
+++ src/main/java/com/malikh/StockWatchdog/dto/UserRegistrationRequest.java
@@ -1,10 +1,8 @@
 package com.malikh.stockwatchdog.dto;
 import lombok.Data;
-import jakarta.validation.constraints.Email;
 @Data
 public class UserRegistrationRequest {
     private String username;
-    @Email
     private String email;
     
     private String password;
