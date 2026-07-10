f7ca63d2b6070e0c0ece6fbd98dff8642455de89
Malik Halabi <malik.halabi@students.mq.edu.au>
2026-07-10 13:39:41 +1000

    Changed repository type to optional to allow for .isEmpty and .isPresent

--- src/main/java/com/malikh/StockWatchdog/Repository/StockRepository.java
+++ src/main/java/com/malikh/StockWatchdog/Repository/StockRepository.java
@@ -1,5 +1,7 @@
 package com.malikh.stockwatchdog.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,6 @@ import com.malikh.stockwatchdog.entity.Stock;
 
 @Repository
 public interface StockRepository extends JpaRepository<Stock, Long> {
+    public Optional<Stock> findStockBySymbol(String symbol);
     
 }
